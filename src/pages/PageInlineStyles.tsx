921187f73011b1d2b2a56853a374ce183a036687
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-16 11:37:28 +0100

    inline toggle button finished

--- src/pages/PageInlineStyles.tsx
+++ src/pages/PageInlineStyles.tsx
@@ -2,14 +2,10 @@ import { useState } from "react";
 
 export const PageInlineStyles = () => {
 	const [isOnline, setIsOnline] = useState(true);
-	const handleButtonClick = () => {
-		setIsOnline(!isOnline)
-	}
-		
 	return (
 		<>
 			<p>This is the inline styles page.</p>
-			<button onClick={handleButtonClick}>Toggle</button>
+			<button onClick={() => setIsOnline(!isOnline)}>Toggle</button>
 			<p style={{ color: isOnline ? "green" : "red" }}>
 				{isOnline ? "User is now online" : "User is now offline"}
 			</p>
