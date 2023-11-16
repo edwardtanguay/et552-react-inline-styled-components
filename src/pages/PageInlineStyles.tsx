37e63cb75c6186d98a7fdd2e06e5ad6060dd7b7d
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-16 11:34:16 +0100

    toggle finished

--- src/pages/PageInlineStyles.tsx
+++ src/pages/PageInlineStyles.tsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
 export const PageInlineStyles = () => {
+	const [isOnline, setIsOnline] = useState(true);
+	const handleButtonClick = () => {
+		setIsOnline(!isOnline)
+	}
+		
 	return (
-		<p>This is the inline styles page.</p>
-	)
-}
+		<>
+			<p>This is the inline styles page.</p>
+			<button onClick={handleButtonClick}>Toggle</button>
+			<p style={{ color: isOnline ? "green" : "red" }}>
+				{isOnline ? "User is now online" : "User is now offline"}
+			</p>
+			<p style={{fontWeight: isOnline ? 'bold': 'normal'}}>
+				Hans Schmidt 
+			</p>
+		</>
+	);
+};
